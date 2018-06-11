04418bf7b4e882fd63005783014c94661da45fed
Giorgian Borca-Tasciuc <giorgian.borca-tasciuc@stonybrook.edu>
2018-06-11 14:44:35 -0400

    Preliminary features working

--- merge_trees.cpp
+++ merge_trees.cpp
@@ -6,65 +6,79 @@
 #include <getopt.h>
 #include <string>
 
-enum command_line_options
-{
-	option_detector = 0,
+#include "TFile.h"
+#include "TChain.h"
+
+enum command_line_options {
+	option_detector = 1,
+	option_out,
+	option_tree,
 };
 
+void process_directory(TChain *const t, const char *const detector, const char *const directory);
 
-void process_directory(const char *const detector, const char *const directory);
-
-	int
-main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	static const struct option long_options[] {
-		{"detector", required_argument, NULL, option_detector},
-			{0, 0, 0, 0}
+		{"detector", required_argument, NULL, option_detector}, 
+		{"out", required_argument, NULL, option_out}, 
+		{"tree", required_argument, NULL, option_tree}, 
+		{0, 0, 0, 0}
 	};
 
-	const char *detector = NULL;
+	const char *detector = "cemc";
+	const char *out = "Merged.root";
+	const char *tree = "ntp_cluster";
 
-	for (;;)
-	{
+	for (;;) {
 		int option_index = 0;
 
-		int c = getopt_long (argc, argv, "", long_options, &option_index);
+		int c =
+		    getopt_long(argc, argv, "", long_options, &option_index);
 		if (c == -1)
 			break;
 
-		switch (c)
-		{
-			case option_detector:
-				detector = optarg;
-				break;
+		switch (c) {
+		case option_detector:
+			detector = optarg;
+			break;
+		case option_out:
+			out = optarg;
+			break;
+		case option_tree:
+			tree = optarg;
+			break;
 		}
 	}
-	if (!detector) {
-		std::fputs("Error: must specify a detector name.\n", stderr);
-		std::exit(EXIT_FAILURE);
-	}
+
+	TFile *const f = new TFile(out, "RECREATE");
+	TChain *const t = new TChain(tree);
 
 	if (optind == argc)
-		process_directory (detector, ".");
+		process_directory(t, detector, ".");
 
-	for (int i = optind; i < argc; ++i) 
-		process_directory (detector, argv[i]);
+	for (int i = optind; i < argc; ++i)
+		process_directory(t, detector, argv[i]);
 }
 
-void process_directory(const char *const detector, const char *const directory)  {
-	std::printf("Looking through: %s\n", directory);
+void process_directory(TChain *const t, const char *const detector, const char *const directory)
+{
 
-	std::regex root_file_pattern {".*" + std::string (detector) + ".*"};
-	DIR *cwd = opendir (directory);
-	if (!cwd) {
-		std::fputs("Error NULL\n", stderr);
-		exit(EXIT_FAILURE);
+	std::regex root_file_pattern {".*" + std::string(detector) + ".*"};
+
+	DIR *const dir = opendir(directory);
+	if (!dir) {
+		std::fprintf(stderr, "Error: Could not open %s\n", directory);
+		return;
 	}
-	struct dirent *d;
 
+	std::printf("Looking through: %s\n", directory);
+	const struct dirent *d;
+	while (d = readdir(dir))
+		if (std::regex_match(d->d_name, root_file_pattern)) {
+			std::printf("Added: %s\n", d->d_name);
+			t->Add(d->d_name);
+		}
 
-	while (d = readdir (cwd))
-		if (std::regex_match (d->d_name, root_file_pattern))
-			std::printf ("%s matches\n", d->d_name);
+	closedir(dir);
 }
-
