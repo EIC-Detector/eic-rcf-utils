2310da18f2fcbf0438cb473d00a51ddf34058c55
Giorgian Borca-Tasciuc <giorgian.borca-tasciuc@stonybrook.edu>
2018-06-11 16:31:22 -0400

    Added command-line options

--- run-particle-gun.sh
+++ run-particle-gun.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 # Before running this file, update the following IN THIS FILE:
-# MACROS_DIRECTORY
-# NEVENTS
-# BATCH
-# EMAIL
+# MACROS_DIRECTORY (although you can pass this in the command-line argument -m or --macros-directory)
+# NEVENTS (although you can pass this in the command-line argument -n or --number-events)
+# BATCH	(although you can pass this in the command-line argument -b or --batch)
+# EMAIL (although you can pass this in the command-line argument -e or --email)
 # Before running this file, update the following in Fun4All_G4_EICDetector.C:
 # Set usegun to true
 # Within the if (usegun) block
@@ -14,7 +13,15 @@
 #	Update set_eta_range to your desired eta range (angle particle is shot at perpendicular to the beamline)
 #	Update set_phi_range to your desired phi range (angle particle is shot at looking at the beamline)
 
-# Directory where g4 Fun4AllSimulations are
+# Usage: run-particle-gun.sh [OPTIONS]...
+# -m,--macros-directory			specifies directory where g4simulation Fun4AllMacroes are
+# -n,--number-events			specifies number of events to run
+# -b,--batch				specifies how many events to run per batch
+# -e,--email				specifies the email that condor emails once jobs are done
+# -r,--results-directory			specifies which directory to store the results in
+# -h,--help				displays this message
+
+# Directory where g4simulation Fun4AllSimulations are
 MACROS_DIRECTORY=/direct/sphenix+u/$USER/macros/macros/g4simulations
 EMAIL='my_email@my_domain.my_ext' # email that condor emails user when jobs are done
 
@@ -23,6 +30,35 @@ DNAME=particle-gun-simulation-$(date +%F-%T)
 NEVENTS=1 # number of events to run
 BATCH=1 # Run the events in batches of $BATCH 
 # if you have 100 events that you run in batches of 1, 100 condor jobs will be created running each event in parallel
+
+TEMP=`getopt -o m:n:b:e:r:h --long macros-directory:,number-events:,batch:,email:,results-directory:,help \
+     -n 'run-particle-gun.sh' -- "$@"`
+
+if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+eval set -- "$TEMP"
+while true ; do
+    case "$1" in
+        -m|--macros-directory) MACROS_DIRECTORY=$2; shift 2 ;;
+        -n|--number-events) NEVENTS=$2; shift 2 ;;
+        -b|--batch) BATCH=$2 ; shift 2 ;;
+        -e|--email) EMAIL=$2; shift 2 ;;
+        -r|--results-directory) DNAME=$2; shift 2 ;;
+	-h|--help) cat << EOF
+Usage: run-particle-gun.sh [OPTIONS]...
+-m,--macros-directory			specifies directory where g4simulation Fun4AllMacroes are
+-n,--number-events			specifies number of events to run
+-b,--batch				specifies how many events to run per batch
+-e,--email				specifies the email that condor emails once jobs are done
+-r,--results-directory			specifies which directory to store the results in
+-h,--help				displays this message
+EOF
+		exit 0
+		;;
+
+	--) shift; break ;;
+        *) echo "Internal error!" >&2 ; exit 1 ;;
+    esac
+done
 
 mkdir $DNAME
 cd $DNAME
