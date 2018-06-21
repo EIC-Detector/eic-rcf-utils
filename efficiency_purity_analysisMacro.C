feb106ceeb1e4e96cd261aab659ff35896d7e4c1
GregoryMatousek <gregory.matousek@stonybrook.edu>
2018-06-21 15:31:17 -0400

    Updated

--- efficiency_purity_analysisMacro.C
+++ efficiency_purity_analysisMacro.C
@@ -123,6 +123,7 @@ double get_true_positive(TString detector, TString cut, int GeV)
   TTree *t_electron =(TTree*)f1->Get("event_cluster");
 
   double truePositive = t_electron->GetEntries(cut)/10000.0;
+  cout << truePositive << endl;
   //f1->Close(); delete t_electron; t_electron=NULL;
   delete t_electron; t_electron=NULL; f1->Close();
   return truePositive;
