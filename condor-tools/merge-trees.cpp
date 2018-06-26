709b6191a80c2e9e0b2a0096a546508c4e96818f
Giorgian Borca-Tasciuc <giorgian.borca-tasciuc@stonybrook.edu>
2018-06-26 11:26:22 -0400

    Added support for merging all trees in a file without explicit passing of all the trees

--- condor-tools/merge-trees.cpp
+++ condor-tools/merge-trees.cpp
@@ -1,10 +1,11 @@
 /* To compile this program, run:
  * g++ -std=c++11 -o merge-trees merge-trees.cpp `root-config --cflags --libs`
- * Usage: merge-trees [OPTIONS]... [FILE]...
+ * Usage: merge-trees [OPTION]... [FILE]...
  * Merge trees from specified FILEs into a single ROOT file.
  *
  * --out, --out=		path where to save merged trees
  * --tree, --tree=		which tree to read from files
+ * --all			merge all trees found in first file passed (default behaviour)
  * --help			print this message
 */
 #include <iostream>
@@ -18,6 +19,7 @@
 enum command_line_options {
 	option_out = 1,
 	option_tree,
+	option_all,
 	option_help,
 };
 
@@ -26,6 +28,7 @@ int main(int argc, char *argv[])
 	static const struct option long_options[] {
 		{"out", required_argument, NULL, option_out}, 
 		{"tree", required_argument, NULL, option_tree}, 
+		{"all", no_argument, NULL, option_all}, 
 		{"help", no_argument, NULL, option_help}, 
 		{0, 0, 0, 0}
 	};
@@ -33,6 +36,7 @@ int main(int argc, char *argv[])
 	const char *out = "Merged.root";
 	std::vector < const char *>trees;
 
+	bool merge_all_specified = false;
 	for (;;) {
 		int option_index = 0;
 
@@ -46,14 +50,20 @@ int main(int argc, char *argv[])
 			out = optarg;
 			break;
 		case option_tree:
-			trees.push_back(optarg);
+			if (!merge_all_specified)
+				trees.push_back(optarg);
+			break;
+		case option_all:
+			merge_all_specified = true;
+			trees.clear();
 			break;
 		case option_help:
 			const char *const help_message =
-			    "Usage: merge-trees [OPTIONS]... [FILE]...\n"
+			    "Usage: merge-trees [OPTION]... [FILE]...\n"
 			    "Merge trees from specified FILEs into a single ROOT file.\n\n"
 			    "--out, --out=\t\t\tpath where to save merged trees\n"
 			    "--tree, --tree=\t\t\twhich tree to read from files\n"
+			    "--all          \t\t\tmerge all trees found in first file passed (default behaviour)\n"
 			    "--help\t\t\t\tprint this message\n";
 			std::cout << help_message;
 			std::exit(EXIT_SUCCESS);
@@ -65,18 +75,27 @@ int main(int argc, char *argv[])
 		std::exit(EXIT_FAILURE);
 	}
 
-	if (trees.size() == 0)
-		trees.push_back("ntp_cluster");
+	/* Add all trees from first file passed */
+	if (trees.size() == 0) {
+		if (!merge_all_specified)
+			std::
+			    cout <<
+			    "No trees specified, will merge all trees found in first file passed.\n";
+
+		TFile *const f = new TFile(argv[optind]);
+		const TList *const ftrees = f->GetListOfKeys();
+		const int ntrees = ftrees->GetSize();
+		for (int i = 0; i < ntrees; ++i)
+			trees.push_back(ftrees->At(i)->GetName());
+	}
 
 	TFile *const f = new TFile(out, "RECREATE");
-
 	for (size_t i = 0; i < trees.size(); ++i) {
 		TChain *const t = new TChain(trees[i]);
 		for (int j = optind; j < argc; ++j) {
 			t->Add(argv[j]);
-			std::
-			    cout << "Added (" << trees[i] << "): " << argv[j] <<
-			    '\n';
+			std::cout << "Added (" << trees[i] << "): " << argv[j]
+			    << '\n';
 		}
 
 		t->CloneTree(-1, "fast");
