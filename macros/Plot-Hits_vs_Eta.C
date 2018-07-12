93f0b0d673bdcfd3b5e2d9bed92e2942d8d51e38
Giorgian Borca-Tasciuc <giorgian.borca-tasciuc@stonybrook.edu>
2018-07-12 17:06:30 -0400

    Finished plot

--- macros/Plot-Hits_vs_Eta.C
+++ macros/Plot-Hits_vs_Eta.C
@@ -7,6 +7,7 @@
 #include <set>
 #include <cmath>
 #include <utility>
+#include <cassert>
 
 #include "TFile.h"
 #include "TTree.h"
@@ -29,13 +30,22 @@ TTree *load_tree(const char *const file_name, const char *const tree_name);
 const char *const svtx_file_path {"/sphenix/user/giorgian/hits-per-eta/hitcount.root"};
 
 const char *const hit_containers[] {"G4HIT_EGEM_0", "G4HIT_EGEM_1", "G4HIT_EGEM_3", 
-	"G4HIT_FGEM_0", "G4HIT_FGEM_1", "G4HIT_FGEM_2", "G4HIT_FGEM_3","G4HIT_FGEM_4", "G4HIT_MAPS", 
+	"G4HIT_FGEM_0", "G4HIT_FGEM_1", "G4HIT_FGEM_2", "G4HIT_FGEM_3","G4HIT_FGEM_4", "G4HIT_MAPS",
 	"G4HIT_SVTX"};
+const static Color_t plot_colors[] = { kBlue, kSpring, kBlack, kYellow, kPink, kGray, kMagenta, kOrange,
+	kRed, kCyan, kViolet};
+
 
 void Plot_Hit_Count() {
+	assert(NELEMS(plot_colors) >= NELEMS(hit_containers));
 	SetsPhenixStyle();
 	gROOT->SetBatch(true);
 
+	TCanvas *const c {new TCanvas {"hits", "Hits", gStyle->GetCanvasDefW(), gStyle->GetCanvasDefH()}};
+	TLegend *const l {new TLegend {0.7, 0.9, 0.95, 0.6, "Detector"}};
+	l->SetTextSize(0.03);
+	Double_t max {0};
+	std::vector<TH1F*> hists {};
 	for (size_t i {0}; i < NELEMS(hit_containers); ++i) {
 		const std::string tree_name {std::string(hit_containers[i]) + "_normalized"};
 		TTree *const hits {load_tree(svtx_file_path, tree_name.c_str())};
@@ -46,29 +56,41 @@ void Plot_Hit_Count() {
 		hits->SetBranchAddress("hit_count", &hit_count);
 
 		Long64_t nentries {hits->GetEntries()};
-		TH1F h {hit_containers[i], "Hit Count", 100, -4.5, 4.5};
+		TH1F *const h {new TH1F {hit_containers[i], "Hit Count", 100, -4.5, 4.5}};
+		h->SetLineColor(plot_colors[i]);
 		for (Long64_t i {0}; i < nentries; ++i) {
 			if (hits->LoadTree(i) < 0)
 				break;
 			hits->GetEntry(i);
-			h.Fill(eta, hit_count);
+			h->Fill(eta, hit_count);
+		}
+
+		const Long64_t nbins {h->GetSize() - 2};	/* -2 for underflow and overflow */
+		for (Long64_t j {0}; j < nbins; ++j) {
+			const Double_t hit_count {h->GetBinContent(j + 1)};
+			max = (hit_count > max) ? hit_count : max;
 		}
 
 
-		TCanvas c {"hits", "Hits", gStyle->GetCanvasDefW(), gStyle->GetCanvasDefH()};
-		h.Draw();
-
-		TLegend l {0.65, 0.9, 0.95, 0.8, "Detector"};
-		l.SetTextSize(0.03);
-		l.AddEntry(&h, hit_containers[i], "l");
-		l.Draw();
-		gPad->RedrawAxis();
-
-
-		TImage *const img {TImage::Create()};
-		img->FromPad(&c);
-		img->WriteImage((std::string(hit_containers[i]) + ".png").c_str());
+		hists.push_back(h);
+		l->AddEntry(h, hit_containers[i], "l");
 	}
+	
+	for (const auto& h: hists) {
+		c->cd();
+		h->GetYaxis()->SetRangeUser(0, max * 1.1);
+		h->Draw("SAME");
+
+	}
+
+	l->Draw();
+	gPad->RedrawAxis();
+
+
+	TImage *const img {TImage::Create()};
+	img->FromPad(c);
+	img->WriteImage("Hits_vs_Eta.png");
+
 	gApplication->Terminate(0);
 }
 
